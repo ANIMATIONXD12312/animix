0953ded558ea33b8d2bc3f128d3b37e266f256eb
ANIMATIONXD12312 <162057372+ANIMATIONXD12312@users.noreply.github.com>
2026-03-22 16:34:30 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -568,7 +568,7 @@ const server = http.createServer(async (req, res) => {
         });
 
         const groqBody = JSON.stringify({
-          model: 'llama-3.2-90b-vision-preview',
+          model: 'meta-llama/llama-4-scout-17b-16e-instruct',
           messages: openaiMessages,
           max_tokens: 4096,
           temperature: 0.9,
