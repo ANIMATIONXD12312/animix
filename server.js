c01b01e0839892070ad6d018ac1f3892ffad7187
ANIMATIONXD12312 <162057372+ANIMATIONXD12312@users.noreply.github.com>
2026-03-27 12:10:58 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 function wait(ms) { return new Promise(r => setTimeout(r, ms)); }
 
